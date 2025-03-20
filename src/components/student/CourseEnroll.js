097eacd6f38d7ee23af17db2bc9c6211841d8dea
Polina <zhperon@gmail.com>
2025-03-20 13:32:36 -0700

    Enrolling and unenrolling done under "Enroll in a class" page

--- src/components/student/CourseEnroll.js
+++ src/components/student/CourseEnroll.js
@@ -16,45 +16,98 @@ import {SERVER_URL} from '../../Constants';
 const CourseEnroll = (props) => {
     const [openSections, setOpenSections] = useState([])
     const [enrollingSection, setEnrollingSection] = useState({})
+    const [unEnrollingSection, setUnEnrollingSection] = useState({})
     const [openConfirmEnrollment, setOpenConfirmEnrollment] = useState(false)
+    const [openConfirmUnEnrollment, setOpenConfirmUnEnrollment] = useState(false)
+    const [currentEnrollments, setCurrentEnrollments] = useState([])
     const tableHeaders = ["Course Id", "Title", "Semester", "Days+Times", "Instructor Name", "Instructor Email", "Building", "Room", "Sec No", ""]
     const getOpenSections = async()=>{
         try{
             let response = await fetch(`${SERVER_URL}/sections/open`)
             const courses = await response.json()
-            if(response.status == 200){
+            if(response.status === 200){
                 setOpenSections(courses)
+                console.log(courses)
             }else{
-                alert(`Error ${response.status}\nProblem retrieving sections`)
+                alert(`Status ${response.status}\nProblem retrieving sections`)
             }
         }catch(err){
             alert(`${err}`)
         }
         
     }   
+
+    const getMyEnrollments = async()=>{
+        try{
+            let response  = await fetch(`${SERVER_URL}/transcripts?studentId=3`)
+            if (response.status === 200){
+                let data = await response.json()
+                setCurrentEnrollments(data)
+                console.log(data)
+            }else{
+                alert(`Status ${response.status}\nProblem retrieving sections`)
+            }
+        }catch(err){
+            alert(`${err}`)
+        }
+    }
     const enrollMe = async(secNo)=>{
         try{
-            let response = await fetch(`${SERVER_URL}/enrollments/sections/${secNo}?studentId=3`)
-            
+            let response = await fetch(`${SERVER_URL}/enrollments/sections/${secNo}?studentId=3`, 
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(enrollingSection)
+                }
+            )
+            if (response.status === 200){
+                let status = await response.json()
+                console.log(status)
+                getMyEnrollments()
+            }
         }catch(err){
             alert(`${err}`)
         }
         setOpenConfirmEnrollment(false)
     }
+    const unenrollMe = async()=>{
+        try{
+            let response = await fetch(`${SERVER_URL}/enrollments/${unEnrollingSection.enrollmentId}`, 
+                {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }
+            )
+            getMyEnrollments()
+        }catch(err){
+            console.log("here")
+            alert(`${err}`)
+        }
+        setOpenConfirmUnEnrollment(false)
+        
+    }
     useEffect(()=>{
         getOpenSections()
+        getMyEnrollments()
     }, [])
  
     return(
         <>
            <table className="Center">
+            <thead>
             <tr>
                 {tableHeaders.map((header, idx)=>(
-                    <th id={idx}>{header}</th>
+                    <th key={idx}>{header}</th>
                 ))}
             </tr>
+            </thead>
+            <tbody>
             {openSections.map((s, idx)=>(
-                <tr id={s.secId}>
+                <tr key={s.secNo} id={s.secNo}>
                     <td>{s.courseId}</td>
                     <td>{s.title}</td>
                     <td>{s.semester+" "+s.year}</td>
@@ -64,12 +117,23 @@ const CourseEnroll = (props) => {
                     <td>{s.building}</td>
                     <td>{s.room}</td>
                     <td>{s.secNo}</td>
-                    <td><Button onClick={()=>{
+                    <td>{currentEnrollments.find(enrollment => enrollment.sectionNo == s.secNo)?
+                        <Button onClick={()=>{
+                            const enrollment = currentEnrollments.find(enrollment => enrollment.sectionNo == s.secNo)
+                            setUnEnrollingSection(enrollment)
+                            setOpenConfirmUnEnrollment(true)
+                        }}variant="contained">
+                            Drop
+                        </Button>
+                        :
+                        <Button variant="contained" onClick={()=>{
                         setOpenConfirmEnrollment(true)
                         setEnrollingSection(s)
-                        }}>Enroll</Button></td>
+                        }}>Enroll</Button>
+                    }</td>
                 </tr>
             ))}
+            </tbody>
            </table>
            <Dialog open={openConfirmEnrollment}>
                 <DialogTitle>Are you sure you would like to enroll in {enrollingSection.title} for {enrollingSection.semester+" "+enrollingSection.year}</DialogTitle>
@@ -79,6 +143,17 @@ const CourseEnroll = (props) => {
                     }}>Enroll</Button>
                     <Button color="secondary" onClick={()=>{
                         setOpenConfirmEnrollment(false)
+                    }}>Cancel</Button>
+                </DialogActions>
+           </Dialog>
+           <Dialog open={openConfirmUnEnrollment}>
+                <DialogTitle>Are you sure you would like to drop {unEnrollingSection.title} for {unEnrollingSection.semester+" "+unEnrollingSection.year}</DialogTitle>
+                <DialogActions>
+                    <Button color='primary' onClick={()=>{
+                        unenrollMe(unEnrollingSection.secNo)
+                    }}>Unenroll</Button>
+                    <Button color="secondary" onClick={()=>{
+                        setOpenConfirmUnEnrollment(false)
                     }}>Cancel</Button>
                 </DialogActions>
            </Dialog>
